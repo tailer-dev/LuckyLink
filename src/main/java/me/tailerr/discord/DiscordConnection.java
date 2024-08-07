e43adfebe5a3c1eb700484c20b17ce5e1b0fd8f7
tailer <94867591+tailer-dev@users.noreply.github.com>
2024-08-07 23:29:40 +0100

    Creation of LuckyLink

--- src/main/java/me/tailerr/discord/DiscordConnection.java
+++ src/main/java/me/tailerr/discord/DiscordConnection.java
@@ -1,7 +1,22 @@
 package me.tailerr.discord;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
 public class DiscordConnection {
+    private JDA jda;
 
+    public DiscordConnection(String BOT_TOKEN) {
 
+        try {
+            jda = JDABuilder.create(BOT_TOKEN,
+                            GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGES)
+                    .build().awaitReady();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
