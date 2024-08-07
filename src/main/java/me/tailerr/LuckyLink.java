f83f21e7e25f20ea1cab230363d72c6a9ed0d3eb
tailer <94867591+tailer-dev@users.noreply.github.com>
2024-08-07 23:23:36 +0100

    Creation of LuckyLink

---
+++ src/main/java/me/tailerr/LuckyLink.java
@@ -0,0 +1,17 @@
+package me.tailerr;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LuckyLink extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
